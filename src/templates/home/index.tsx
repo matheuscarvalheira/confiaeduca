6ad5649aa61fa7de810eff18fd33e27ab46959b7
Matheus Carvalheira <matheuscarvalheiraf@gmail.com>
2025-01-28 22:43:04 -0300

    Created CustomHeader component to display a title and subtitle.

--- src/templates/home/index.tsx
+++ src/templates/home/index.tsx
@@ -1,9 +1,10 @@
+import CustomHeader from "@/components/CustomHeader";
 import { FC } from "react";
 
 export const HomeTemplate: FC = () => {
   return (
     <div>
-      olá
+      <CustomHeader title="Bem vindo, professor" subtitle="Selecione a turma desejada" showArrow={true} />
     </div>
   )
 }
