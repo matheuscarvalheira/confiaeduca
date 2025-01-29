7db25b4d12ef122e8f7543e710bcae0c45cecafa
mario <mario.andre.m@gmail.com>
2025-01-28 21:10:39 -0300

    header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
-'use client';
+"use client";
 
 import { StyledComponentsRegistry } from "@/lib/registry";
 import { GlobalStyled } from "@/styles/global";
 import { theme } from "@/styles/theme";
 import { ThemeProvider } from "styled-components";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Header } from "@/components/Header";
 
 export default function RootLayout({
   children,
@@ -18,8 +19,17 @@ export default function RootLayout({
         <StyledComponentsRegistry>
           <ThemeProvider theme={theme}>
             <GlobalStyled />
-              {children}
-              <ToastContainer />
+            <div
+              style={{
+                minHeight: "100vh",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <Header />
+              <main style={{ flexGrow: 1, padding: "20px" }}>{children}</main>
+            </div>
+            <ToastContainer />
           </ThemeProvider>
         </StyledComponentsRegistry>
       </body>
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,47 @@
+'use client';
+import React from "react";
+import styled from "styled-components";
+import { useRouter } from "next/navigation";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 32px;
+  color: white;
+`;
+
+const Logo = styled.a`
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
+`;
+
+const LogoutButton = styled.button`
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
+
+const LogoutIcon = styled.img`
+  
+`
+
+export const Header: React.FC = () => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    console.log("User logged out");
+    // Implement your logout logic (e.g., clear auth token)
+    router.push("/login");
+  };
+
+  return (
+    <HeaderContainer>
+      <Logo onClick={() => router.push("/")}>ConfiaEduca</Logo>
+      <LogoutButton onClick={handleLogout}><LogoutIcon src="./svg/logout.svg" /></LogoutButton>
+    </HeaderContainer>
+  );
+};
